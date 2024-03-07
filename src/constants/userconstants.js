8c2fc2a2968f2d4752eef11305386b1e7915b96c
rajeshrajesh <rajeshmn47@gmail.com>
2024-03-07 09:38:39 +0530

    Update userconstants.js

--- src/constants/userconstants.js
+++ src/constants/userconstants.js
@@ -55,9 +55,9 @@ export const CLEAR_ERRORS = "CLEAR_ERRORS";
 function geturl() {
   const current = process.env.REACT_APP_API;
   if (current == "local") {
-    return "http://localhost:9000";
+    return "https://apk.thepowerplay.co";
   }
-  return "https://backendforpuand-dream11.onrender.com";
+  return "https://apk.thepowerplay.co";
 }
 
 function getfrontendurl() {
