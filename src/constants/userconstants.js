60f7949ef7a6acbdc44f8dabf7c0b664a49e2180
rajeshmn47 <rajeshmn47@gmail.com>
2024-04-12 08:24:34 +0530

    changed server url

--- src/constants/userconstants.js
+++ src/constants/userconstants.js
@@ -58,8 +58,8 @@ function geturl() {
     return "http://localhost:8000";
     //return "https://apk.thepowerplay.co";
   }
-  //return "https://apk.thepowerplay.co";
-  return "https://backendforpuand-dream11.onrender.com";
+  return "https://apk.thepowerplay.co";
+  //return "https://backendforpuand-dream11.onrender.com";
 }
 
 function getfrontendurl() {
